da86c7a0d84ca9691d3cfc999cd0f3f110a7c3b4
sinclare210 <adebayooluwasegun2004@gmail.com>
2025-04-30 01:46:52 +0100

    working

--- src/CrowdFunding.sol
+++ src/CrowdFunding.sol
@@ -2,5 +2,19 @@
 pragma solidity ^0.8.18;
 
 contract CrowdFunding {
-    
+    address public owner;
+    // State variables
+    struct Campaign {
+        address owner;
+        uint256 targetAmount;
+        uint256 deadline;
+        uint256 totalAmountRaised;
+        bool isCompleted;
+    }
+    mapping (uint256 => Campaign) public Campaigns;
+
+    constructor () {
+        owner = msg.sender;
+    }
+
 }
